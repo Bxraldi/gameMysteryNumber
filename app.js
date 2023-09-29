cf701e7fbf70b9a26f9d82952733bf60c2d8c1ea
Bxraldi <138626263+Bxraldi@users.noreply.github.com>
2023-09-28 21:35:19 -0300

    gameMysteryNumber difficulty.js
    Treinando minha lógica, implementei um sistema de dificuldades.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 alert("Welcome to the Mystery Number!");
-let numMax = 100;
+let difficultyChoose = parseInt(prompt(`Choose the difficulty: \n[1]Easy\n[2]Medium\n[3]Hard`));
+let numMax;
+let difficultyEasy = 'Easy';
+let difficultyMedium = 'Medium';
+let difficultyHard = 'Hard';
+
+if (difficultyChoose == 1) {
+    alert(`Chosen Difficulty: ${difficultyEasy}`)
+    numMax = 100;
+} else if (difficultyChoose == 2) {
+    alert(`Chosen Difficulty: ${difficultyMedium}`)
+    numMax = 500;
+} else if (difficultyChoose == 3) {
+    alert(`Chosen Difficulty: ${difficultyHard}`)
+    numMax = 1000;
+}
+
 let numSecret = parseInt(Math.random() * numMax + 1);
 let guess;
 attempts = 1;
-
 
 while (guess != numSecret) {
     guess = prompt(`Enter a number between 1 and ${numMax}: `);
