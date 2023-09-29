506430ffdc2099299c3cff747ecb25026c098198
Bxraldi <138626263+Bxraldi@users.noreply.github.com>
2023-09-28 23:06:03 -0300

    gameMysteryNumber points1.js
    Implementei o primeiro modelo de pontos, onde o usuario começa com um determinado numero de pontos de acordo com a dificuldade escolhida e a cada erro, é subtraido 50 pontos

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-alert("Welcome to the Mystery Number!");
-let difficultyChoose = parseInt(prompt(`Choose the difficulty: \n[1]Easy\n[2]Medium\n[3]Hard`));
-let numMax;
-let difficultyEasy = 'Easy';
-let difficultyMedium = 'Medium';
-let difficultyHard = 'Hard';
+alert('\nWelcome to the Mystery Number!');
+
+let difficultyChoose = parseInt(prompt(`Choose the difficulty: \n[1] Easy\n[2] Medium\n[3] Hard \n`));
+
+let pointsEasy = 500, pointsMedium = 250, pointsHard = 100;
+let numMaxEasy = 100, numMaxMedium = 250, numMaxHard = 500;
+let difficultyEasy = 'Easy', difficultyMedium = 'Medium', difficultyHard = 'Hard';
+let finalPoints;
 
 if (difficultyChoose == 1) {
-    alert(`Chosen Difficulty: ${difficultyEasy}`)
-    numMax = 100;
+    points = pointsEasy;
+    alert(`Chosen Difficulty: ${difficultyEasy}\nYou have ${pointsEasy} points`);
+    numMax = numMaxEasy;
 } else if (difficultyChoose == 2) {
-    alert(`Chosen Difficulty: ${difficultyMedium}`)
-    numMax = 500;
+    points = pointsMedium;
+    alert(`Chosen Difficulty: ${difficultyMedium}\nYou have ${pointsMedium} points`);
+    numMax = numMaxMedium;
 } else if (difficultyChoose == 3) {
-    alert(`Chosen Difficulty: ${difficultyHard}`)
-    numMax = 1000;
+    points = pointsHard;
+    alert(`Chosen Difficulty: ${difficultyHard}\nYou have ${pointsHard} points`);
+    numMax = numMaxHard;
 }
 
 let numSecret = parseInt(Math.random() * numMax + 1);
@@ -22,18 +27,33 @@ attempts = 1;
 
 while (guess != numSecret) {
     guess = prompt(`Enter a number between 1 and ${numMax}: `);
-    if (numSecret == guess) {
-        break
+
+    if (numSecret == guess || guess === null) {
+
+        break;
+
     } else {
+
         if (numSecret > guess) {
-            alert(`Too bad, the mysterious number is more than ${guess}`);
+            points -= 50;
+            alert(`Too bad, the mysterious number is more than [${guess}]\nYou lost 50 points\n\n\nYour points: ${points}`);
         } else {
-            alert(`Too bad, the mysterious number is less than  ${guess}`);
+            points -= 50;
+            alert(`Too bad, the mysterious number is less than  [${guess}]\nYou lost 50 points\n\n\nYour points: ${points}`);
         }
         attempts++
+        finalPoints = points;
+    }
+
+    if (points == 0) {
+        alert('You lost.');
+        break;
+    } else {
+        continue;
     }
 }
 
 let wordTry = attempts > 1 ? 'tries' : 'try';  //string.format
-alert(`Great! You guessed the mystery number! [${numSecret}] with ${attempts} ${wordTry}`);
+alert(`Great! You guessed the mystery number! [${numSecret}] with ${attempts} ${wordTry}\nYour final points: ${finalPoints}`);
+
 
